9568b11c40550ab8bf1ec554f9c8cc1f449e0fb9
Avijit Lodh <avijitklodh@gmail.com>
2019-07-22 23:37:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,9 +15,11 @@ def find_max_value(array)
     i = 0 
   while i < array.length do 
     if array[i] > array[(i+1)]
+      return i
+    elsif
       i = i+1
     end
-    array [i]
+    return array [i]
   end
 end
 def find_min_value(array)
